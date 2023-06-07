de64b38e07b82c5f0b9fce99392880b5a0e78a06
vikash <vikash@ip-192-168-1-16.ap-south-1.compute.internal>
2023-06-07 11:07:46 +0530

    added route for logout user

--- src/routers/user.js
+++ src/routers/user.js
@@ -9,7 +9,7 @@ router.post("/users", async (req, res) => {
 
   try {
     await user.save();
-    const token = user.generateJsonWebToken();
+    const token = await user.generateJsonWebToken();
     res.status(201).send({ user, token });
   } catch (error) {
     res.status(400).send(error);
@@ -22,14 +22,37 @@ router.post("/users/login", async (req, res) => {
       req.body.email,
       req.body.password
     );
-    const token = user.generateJsonWebToken();
+    const token = await user.generateJsonWebToken();
     if (!user) {
       res.status(400);
       return;
     }
+    console.log("token", token);
     res.send({ user, token });
   } catch (error) {
     res.status(400).send(error);
+  }
+});
+
+router.post("/users/logout", auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter(
+      (token) => token.token !== req.token
+    );
+    await req.user.save();
+    res.send();
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+router.post("/users/logoutAll", auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+    res.send();
+  } catch (error) {
+    res.status(500).send();
   }
 });
 
